2d7d4f5222557b6c8260706b2a79105744380033
MarcoCatoi <catoi.work@gmail.com>
2025-12-02 17:38:04 +0000

    app.py -» foi retirado as impressoes "print" para verificação de erro.

--- app/app.py
+++ app/app.py
@@ -9,37 +9,23 @@ from PySide6.QtWidgets import QApplication
 from app.gui.overlay import Overlay
 from app.gui.bubble import TranslationBubble
 from app.gui.floating_button import FloatingButton
-#from app.gui.exit_button import ExitButton
 from app.core.capture import grab_region
 from app.core.preprocess import enhance_for_ocr
 from app.core.ocr import image_to_text
 from app.core.translate import translate_en_to_pt
 
 
-print(">>> app.app foi importado")
-
-
 class FloatingTranslateApp:
     def __init__(self) -> None:
-        print("Init FloatingTranslateApp...")
-
         self.panel = TranslationBubble()
-        print("Panel criado")
-        self.panel.show()
-
+        
         self.overlay = Overlay()
-        print("Overlay criado")
         self.overlay.selection_made.connect(self.on_selection_made)
-        print("Sinal conectado")
 
         self.floating_button = FloatingButton()
         self.floating_button.clicked_for_selection.connect(self.show_overlay)
         self.floating_button.quit_requested.connect(self.quit)
         self.floating_button.show()
-
-        #self.exit_button = ExitButton()
-        #self.exit_button.quit_requested.connect(self.quit)
-        #self.exit_button.show()
 
     def quit(self) -> None:
         self.overlay.close()
@@ -51,7 +37,6 @@ class FloatingTranslateApp:
         self.overlay.show_overlay()
 
     def on_selection_made(self, bbox: Tuple[int, int, int, int]) -> None:
-        print("BBox recebido:", bbox)
         try:
             img = grab_region(bbox)
             img_proc = enhance_for_ocr(img, scale=1.8)
@@ -66,19 +51,11 @@ class FloatingTranslateApp:
 
 
 def main() -> None:
-    print("Iniciando QApplication...")
     app = QApplication(sys.argv)
-
-    print("Criando FloatingTranslateApp...")
     ft_app = FloatingTranslateApp()
-    print("FloatingTranslateApp criado:", ft_app)
-
-    print("Entrando no loop Qt...")
     rc = app.exec()
-    print("Loop Qt terminou com código:", rc)
     sys.exit(rc)
 
 
 if __name__ == "__main__":
-    print("Chamando main() dentro de app.app")
     main()
